8195b10e207684faa544f86676f9513ada70981a
alireza sayyah <sayyah.alireza@gmail.com>
2022-05-09 01:25:12 +0430

    Add persian translation project

---
+++ PersianTranslation/DataAnnotations/PersianRequiredAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersianTranslation.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public class PersianRequiredAttribute : RequiredAttribute
+    {
+        public override string FormatErrorMessage(string name)
+        {
+            return $"وارد {name} الزامی است";
+        }
+    }
+}
