5cad6fa70ec0139a39c135db3e0bb585705bace5
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2022-06-06 12:26:36 +0430

    blog1

---
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/BlogCategories/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Entities.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ECommerce.Services.IServices;
+
+namespace Bolouri.Areas.Admin.Pages.BlogCategories;
+
+public class CreateModel : PageModel
+{
+    private readonly IBlogCategoryService _blogCategoryService;
+
+    public CreateModel(IBlogCategoryService blogCategoryService)
+    {
+        _blogCategoryService = blogCategoryService;
+    }
+
+    [BindProperty] public BlogCategory BlogCategory { get; set; }
+
+    [TempData] public string Message { get; set; }
+
+    [TempData] public string Code { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _blogCategoryService.Add(BlogCategory);
+            if (result.Code == 0)
+                return RedirectToPage("/BlogCategories/Index",
+                    new {area = "Admin", message = result.Message, code = result.Code.ToString()});
+            Message = result.Message;
+            Code = result.Code.ToString();
+            ModelState.AddModelError("", result.Message);
+        }
+
+        return Page();
+    }
+}
