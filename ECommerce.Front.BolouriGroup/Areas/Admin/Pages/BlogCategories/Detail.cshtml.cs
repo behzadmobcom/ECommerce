bc15d4ccb5d378d6f8f310362b93ec34a6385f22
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2022-06-06 13:04:31 +0430

    blog2

---
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/BlogCategories/Detail.cshtml.cs
@@ -0,0 +1,31 @@
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ECommerce.Services.IServices;
+
+namespace Bolouri.Areas.Admin.Pages.BlogCategories;
+
+public class DetailModel : PageModel
+{
+    private readonly IBlogCategoryService _blogcategoryService;
+
+    public DetailModel(IBlogCategoryService blogcategoryService)
+    {
+        _blogcategoryService = blogcategoryService;
+    }
+
+    public BlogCategory BlogCategory { get; set; }
+
+    public async Task<IActionResult> OnGet(int id)
+    {
+        var result = await _blogcategoryService.GetById(id);
+        if (result.Code == 0)
+        {
+            BlogCategory = result.ReturnData;
+            return Page();
+        }
+
+        return RedirectToPage("/BlogCategories/Index",
+            new {area = "Admin", message = result.Message, code = result.Code.ToString()});
+    }
+}
---
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/BlogCategories/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ECommerce.Services.IServices;
+
+namespace Bolouri.Areas.Admin.Pages.BlogCategories;
+
+public class EditModel : PageModel
+{
+    private readonly IBlogCategoryService _blogcategoryService;
+
+    public EditModel(IBlogCategoryService blogcategoryService)
+    {
+        _blogcategoryService = blogcategoryService;
+    }
+
+    [BindProperty] public BlogCategory BlogCategory { get; set; }
+    [TempData] public string Message { get; set; }
+    [TempData] public string Code { get; set; }
+
+    public async Task OnGet(int id)
+    {
+        var result = await _blogcategoryService.GetById(id);
+        BlogCategory = result.ReturnData;
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _blogcategoryService.Edit(BlogCategory);
+            Message = result.Message;
+            Code = result.Code.ToString();
+            if (result.Code == 0)
+                return RedirectToPage("/BlogCategories/Index",
+                    new {area = "Admin", message = result.Message, code = result.Code.ToString()});
+            Message = result.Message;
+            Code = result.Code.ToString();
+            ModelState.AddModelError("", result.Message);
+        }
+
+        return Page();
+    }
+}
