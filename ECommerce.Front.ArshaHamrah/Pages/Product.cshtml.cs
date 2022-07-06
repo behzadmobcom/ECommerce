b802d5e1bacd95045f3d7f37f92c11b8e222689a
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2022-07-07 01:13:20 +0430

    arsha

--- ECommerce.Front.ArshaHamrah/Pages/Product.cshtml.cs
+++ ECommerce.Front.ArshaHamrah/Pages/Product.cshtml.cs
@@ -2,51 +2,47 @@ using Entities.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ECommerce.Services.IServices;
+using Entities;
+using Entities.Helper;
 
 namespace ArshaHamrah.Pages;
 
 public class ProductModel : PageModel
 {
+   private readonly IProductAttributeGroupService _productAttributeGroupService;
     private readonly ICartService _cartService;
     private readonly IProductService _productService;
     private readonly IStarService _starService;
 
-    public ProductModel(IProductService productService, IStarService starService, ICartService cartService)
+    public ProductModel(IProductService productService, IStarService starService, ICartService cartService,
+       IProductAttributeGroupService productAttributeGroupService)
     {
         _productService = productService;
         _starService = starService;
         _cartService = cartService;
+        _productAttributeGroupService = productAttributeGroupService;
     }
 
     public ProductViewModel Product { get; set; }
     public List<ProductIndexPageViewModel> RelatedProduct { get; set; }
     public double Stars { get; set; }
+    public List<ProductAttributeGroup> AttributeGroups { get; set; }
 
-    public async Task OnGetAsync(string productUrl)
+    public async Task OnGet(string productUrl)
     {
         var resultProduct = await _productService.GetProduct(productUrl);
-        if (resultProduct.Code == 0) Product = resultProduct.ReturnData;
+        if (resultProduct.Code > 0) return;
+        Product = resultProduct.ReturnData;
+        var result = await _productAttributeGroupService.GetByProductId(Product.Id);
+        if (result.Code == ServiceCode.Success)
+            AttributeGroups = result.ReturnData.Where(x =>
+                x.Attribute.Any(a =>
+                    a.AttributeValue.Any(i =>
+                        i.Value != null))).ToList();
+
 
         RelatedProduct = (await _productService.TopRelatives(Product.Id)).ReturnData;
 
         Stars = await _starService.SumStarsByProductId(Product.Id);
-    }
-
-    public async Task<JsonResult> OnGetAddCart(int id, int priceId)
-    {
-        var result = await _cartService.Add(HttpContext, id,  priceId);
-        return new JsonResult(result);
-    }
-
-    public async Task<JsonResult> OnGetLoadCart(int id)
-    {
-        var result = await _cartService.Load(HttpContext);
-        return new JsonResult(result);
-    }
-
-    public async Task<JsonResult> OnGetDeleteCart(int id, int productId, int priceId)
-    {
-        var result = await _cartService.Delete(HttpContext, id,  productId, priceId);
-        return new JsonResult(result);
     }
 }
