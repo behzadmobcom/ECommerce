9ebfa94737da21637f067ae871ec928a70352606
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2022-07-26 12:29:40 +0430

    factor

---
+++ ECommerce.Front.ArshaHamrah/Areas/Admin/Pages/Factors/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using Entities;
+using Entities.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ECommerce.Services.IServices;
+
+namespace ArshaHamrah.Areas.Admin.Pages.Factors;
+
+public class IndexModel : PageModel
+{
+ 
+
+    public IndexModel()
+    {
+        
+    }
+
+   
+    [TempData] public string Message { get; set; }
+
+    [TempData] public string Code { get; set; }
+
+    public async Task<IActionResult> OnGet(string search = "", int pageNumber = 1, int pageSize = 10,
+        string message = null, string code = null)
+    {
+        Message = message;
+        Code = code;
+    
+
+        return RedirectToPage( );
+    }
+}
