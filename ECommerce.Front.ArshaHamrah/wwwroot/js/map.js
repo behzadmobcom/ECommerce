9adfa93a61b818ff433084c6206b459735feef23
alireza sayyah <sayyah.alireza@gmail.com>
2022-08-10 23:53:02 +0430

    [Arsha] change location

--- ECommerce.Front.ArshaHamrah/wwwroot/js/map.js
+++ ECommerce.Front.ArshaHamrah/wwwroot/js/map.js
@@ -6,7 +6,7 @@
         var mapOptions = {
             zoom: 15,
             scrollwheel: false,
-            center: new google.maps.LatLng(37.2736469, 49.5936615)
+            center: new google.maps.LatLng(37.2780605, 49.5939138)
         };
 
         var map = new google.maps.Map(document.getElementById("googleMap"),
