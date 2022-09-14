4bae97b532086058957440e1786cb62692e6aa70
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2022-09-14 19:16:02 +0430

    [boloori] purchase

--- ECommerce.Front.BolouriGroup/Areas/Admin/Pages/PurchaseOrder.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/PurchaseOrder.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Bolouri.Areas.Admin.Pages;
 
-public class BlogCategoryModel : PageModel
+public class PurchaseOrderModel : PageModel
 {
     public void OnGet()
     {
