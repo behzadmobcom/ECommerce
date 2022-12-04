adf3ca1717e52838bdc0dafcfaf7cee3debaf9c2
GolnazNajafi <blume.sein@gmail.com>
2022-12-05 01:23:21 +0330

    frontsetting

--- WebApplication/MauiProgram.cs
+++ WebApplication/MauiProgram.cs
@@ -3,12 +3,15 @@ using WebApplication.Services;
 using WebApplication.ViewModels.StartUp;
 using WebApplication.Views.StartUp;
 using WebApplication.Models;
-using FrontSetting = Ecommerce.Entities.Helper.FrontSetting;
 using Ecommerce.Entities.ViewModel;
 using ECommerce.Services.IServices;
 using ECommerce.Services.Services;
 using Microsoft.Extensions.Configuration;
-using Android.Content.Res;
+using Microsoft.Extensions.Configuration.Json;
+using System.Reflection;
+using Android.Telephony.Data;
+
+
 
 namespace WebApplication
 {
@@ -18,26 +21,40 @@ namespace WebApplication
 
         public static MauiApp CreateMauiApp()
         {
+
+
             var builder = MauiApp.CreateBuilder();
+            //var a = Assembly.GetExecutingAssembly();
+            //var stream = a.GetManifestResourceStream("WebApplication.appsettings.json");
+
+            //var config = new ConfigurationBuilder()
+            //.AddJsonStream(stream)
+            //.Build();
+
+            // builder.Configuration.AddConfiguration(config);
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                }) ;
 
-            builder.Services.AddSingleton<ILoginService, LoginService>();
-            builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddScoped(typeof(IEntityService<>), typeof(EntityService<>));
+            builder.Services.AddScoped<IHttpService, HttpService>();
+            //builder.Services.AddSingleton<ILoginService, LoginService>();
+            builder.Services.AddScoped<ICookieService, CookieService>();
+
+            builder.Services.AddScoped<IUserService, UserService>();
             //Views
 
             builder.Services.AddSingleton<LoginPage>();
 
             //View Models
-            builder.Services.AddSingleton<LoginPageViewModel>();
+            builder.Services.AddScoped<LoginPageViewModel>();
 
-            builder.Services.AddSingleton<LoginViewModel>();
-            builder.Services.AddSingleton<SiteSettings>();
+            builder.Services.AddScoped<LoginViewModel>();
+            builder.Services.AddScoped<SiteSettings>();
             //builder.Services.AddSingleton<FrontSettings>();
 
             var _frontSetting = builder.Configuration.GetSection(nameof(FrontSetting)).Get<FrontSetting>();
--- WebApplication/ViewModels/StartUp/LoginPageViewModel.cs
+++ WebApplication/ViewModels/StartUp/LoginPageViewModel.cs
@@ -24,16 +24,16 @@ namespace WebApplication.ViewModels.StartUp
         private string _password;
 
         private readonly IUserService _userService;
-        private readonly ILoginService _loginService;
+        //private readonly ILoginService _loginService;
 
         public LoginPageViewModel(IUserService userService)
         {
             _userService = userService;
         }
-        public LoginPageViewModel(ILoginService loginService)
-        {
-            _loginService = loginService;
-        }
+        //public LoginPageViewModel(ILoginService loginService)
+        //{
+        //    _loginService = loginService;
+        //}
         #region Commands
         [ICommand]
         async void LoginUser()
@@ -50,27 +50,7 @@ namespace WebApplication.ViewModels.StartUp
 
                 await Launcher.Default.OpenAsync("https://boloorico.com");
 
-                //Saving Cookies
-                CookieContainer cookieContainer = new CookieContainer();
-                Uri uri = new Uri("https://boloorico.com", UriKind.RelativeOrAbsolute);
-
-                Cookie cookie = new Cookie
-                {
-                    Name = "DotNetMAUICookie",
-                    Expires = DateTime.Now.AddDays(1),
-                    Value = "My cookie",
-                    Domain = uri.Host,
-                    Path = "/"
-                };
-                cookieContainer.Add(uri, cookie);
-                WebView webView = new WebView
-                {
-                    Source = "https://boloorico.com"
-                };
-
-                webView.Cookies = cookieContainer;
-                webView.Source = new UrlWebViewSource { Url = uri.ToString() };
-
+                                
                 // await SecureStorage.SetAsync(nameof(App.RuturnData), response.ReturnData);
                 //var tokenDetails=await SecureStorage.GetAsync(nameof(App.RuturnData));
 
