5ee2db360ab4524571e6992f94bc5e7c4906dc5a
Elham Khosravi <ekhosravi@gmail.com>
2023-02-11 11:00:26 +0330

    - EmployeeDto Completed

---
+++ Dto/EmployeesDto/EmployeeDto.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Dto.Base;
+
+namespace Dto.EmployeesDtos;
+
+public class EmployeeDto : BaseDto
+{
+    public string? Name { get; set; }
+
+    public string? Title { get; set; }
+
+}
---
+++ Dto/EmployeesDto/EmployeeDto_.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.Entities;
+using ECommerce.Dto.Base;
+
+namespace Dto.EmployeesDtos;
+
+public class EmployeeDto_ : BaseDto
+{
+    public string? Name { get; set; }
+
+    public string? Title { get; set; }
+
+    public DateTime? HireDateTime { get; set; }
+
+    public int Commission { get; set; }
+
+    public bool IsActive { get; set; }
+
+    //ForeignKey
+    public string? CustomerCode { get; set; }
+
+    public string? CustomerCodeCustomer { get; set; }
+
+    public ICollection<PurchaseOrder>? PurchaseOrderApprovedBy { get; set; }
+
+    public ICollection<PurchaseOrder>? PurchaseOrderSubmittedBy { get; set; }
+
+    public ICollection<PurchaseOrder>? PurchaseOrderAccountant { get; set; }
+
+    public int? DepartmentId { get; set; }
+    public Department? Department { get; set; }
+
+    public int? UserRoleId { get; set; }
+    public UserRole? UserRole { get; set; }
+
+    public ICollection<BlogComment>? BlogComments { get; set; }
+
+    public int? HolooCompanyId { get; set; } = 1;
+    public HolooCompany? HolooCompany { get; set; }
+}
