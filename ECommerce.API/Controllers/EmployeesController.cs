a87d257d5ff01c768c11d26338b73488ed61903e
AliMozafari67 <a.mozafari67@gmail.com>
2023-02-15 20:13:31 +0330

    [2023-02-15Api]Change Employees Controller Address

--- ECommerce.API/Controllers/EmployeesController.cs
+++ ECommerce.API/Controllers/EmployeesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.API.Controllers;
 
-[Route("api/[controller]/[action]")]
+[Route("api/[controller]")]
 [ApiController]
 public class EmployeesController : ControllerBase
 {
@@ -23,7 +23,7 @@ public class EmployeesController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> Get([FromQuery] PaginationParameters paginationParameters,
+    public async Task<IActionResult> Get([FromBody] PaginationParameters paginationParameters,
         CancellationToken cancellationToken)
     {
         try
@@ -56,7 +56,7 @@ public class EmployeesController : ControllerBase
         }
     }
 
-    [HttpGet]
+    [HttpGet("GetById/{id}")]
     public async Task<ActionResult<Employee>> GetById(int id, CancellationToken cancellationToken)
     {
         try
@@ -139,7 +139,7 @@ public class EmployeesController : ControllerBase
         }
     }
 
-    [HttpDelete]
+    [HttpDelete("{id}")]
     [Authorize(Roles = "SuperAdmin")]
     public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
     {
