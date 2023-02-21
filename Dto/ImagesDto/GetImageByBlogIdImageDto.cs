4393f933439ca225ac87e6bbac1fb4a0d542ab8c
Elham Khosravi <ekhosravi@gmail.com>
2023-02-21 13:02:15 +0330

    [2023-02-21 Dto] ImageDto Finished

---
+++ Dto/ImagesDto/GetImageByBlogIdImageDto.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.ImagesDtos;
+
+public class GetImageByBlogIdImageDto : BaseDto
+{
+    public string? Name { get; set; }
+
+    public string? Path { get; set; }
+
+    public string? Alt { get; set; }
+
+}
---
+++ Dto/ImagesDto/PostImageDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.Dto.ImagesDtos;
+
+public class PostImageDto  
+{
+    public string? Name { get; set; }
+
+    public string? Path { get; set; }
+
+    public string? Alt { get; set; }
+
+    public int? ProductId { get; set; }
+    public virtual ProductDto? Product { get; set; }
+
+    public int? BlogId { get; set; }
+    public virtual BlogDto? Blog { get; set; }
+}
