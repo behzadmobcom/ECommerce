696744802df9984b8f612750c3ee8438c4c74423
alireza sayyah <sayyah.alireza@gmail.com>
2023-02-22 18:28:35 +0330

    [2023-02-22 Boloori] Zoom + categories side menu

---
+++ ECommerce.Front.BolouriGroup/ViewComponents/CategoriesMobileViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Front.BolouriGroup.ViewComponents
+{
+    public class CategoriesMobileViewComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesMobileViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = (await _categoryService.GetParents()).ReturnData;
+            return View(result);
+        }
+    }
+}
