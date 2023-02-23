201c23b976ecef6605b9e61e19f782f4522e624f
Elham Khosravi <ekhosravi@gmail.com>
2023-02-23 19:38:54 +0330

    [2023-02-23 Dto] PeymentMethod Reviewed

---
+++ Dto/PaymentMethodsDto/GetPaymentMethodDto.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.PaymentMethodsDto;
+
+public class GetPaymentMethodDto : BaseDto
+{
+    public string? AccountNumber { get; set; }
+
+    public string? BankName { get; set; }
+     
+}
---
+++ Dto/PaymentMethodsDto/PostPaymentMethodDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Dto.PaymentMethodsDto;
+
+public class PostPaymentMethodDto  
+{
+    public string? AccountNumber { get; set; }
+
+    public string? BankName { get; set; }
+
+    public string? BrunchName { get; set; }
+
+    public string? BankCode { get; set; }
+     
+} 
