6276ffb97a47e206f3cc96903a1a567a2c14712d
Elham Khosravi <ekhosravi@gmail.com>
2023-03-01 16:49:03 +0330

    2023-03-01_ReviewProductDto

---
+++ Dto/ProductsDto/PriceDto.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.ProductsDto;
+
+public class PriceDto : BaseDto
+{
+    public decimal Amount { get; set; }
+
+    public bool IsColleague { get; set; }
+
+    public double Exist { get; set; }
+
+    public int? ColorId { get; set; } 
+    public ColorDto? Color { get; set; }
+
+    public int? DiscountId { get; set; }
+    public DiscountDto? Discount { get; set; }
+}
---
+++ Dto/ProductsDto/ProductDto.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.ProductsDto;
+
+public class ProductDto : BaseDto
+{
+    public string? Name { get; set; }
+
+    public bool IsShowInIndexPage { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Review { get; set; }
+
+    public double Star { get; set; }
+
+    public int MinOrder { get; set; } 
+
+    public int? MaxOrder { get; set; }
+
+    public double? MinInStore { get; set; } 
+
+    public int? ReorderingLevel { get; set; }
+
+    public bool IsDiscontinued { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? Url { get; set; }
+
+}
