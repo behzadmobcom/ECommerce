c570d2cd079f1789693936de324e6a9abfa215d7
Elham Khosravi <ekhosravi@gmail.com>
2023-03-02 15:29:27 +0330

    [2023-03-02 Dto] Review SettingDto

---
+++ Dto/SettingsDto/SettingDto.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.SettingsDto;
+
+public class SettingDto : BaseDto
+{
+    public string? Name { get; set; }
+
+    public string? Value { get; set; }
+
+}
+
