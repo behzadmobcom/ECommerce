0abb837df92d20c9365c14b2a088faaaa88586d1
Elham Khosravi <ekhosravi@gmail.com>
2023-03-02 15:34:16 +0330

    [2023-03-02 Dto] Review ShippingDto

---
+++ Dto/ShippingsDto/ShippingDto.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Dto.Base;
+
+namespace ECommerce.Dto.ShippingsDto;
+
+public class ShippingDto : BaseDto
+{
+    public string? Name { get; set; }
+    public ICollection<PurchaseOrderDto>? PurchaseOrder { get; set; }
+
+}
