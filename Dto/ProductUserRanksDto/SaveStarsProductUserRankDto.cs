e3654145b66d4a6e4226dfbf46b1679efc4bd86e
Elham Khosravi <ekhosravi@gmail.com>
2023-03-02 16:35:54 +0330

    [2023-03-02 Dto] Review ProductUserRank Dto for adding SaveStarProductUserRankDto Class

---
+++ Dto/ProductUserRanksDto/SaveStarsProductUserRankDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ECommerce.Dto.ProductUserRanksDto;
+
+public class SaveStarsProductUserRankDto  
+{
+    public int Stars { get; set; }
+
+    public int UserId { get; set; }
+
+    public UserDto? User { get; set; }
+
+    public int ProductId { get; set; }
+
+    public ProductDto? Product { get; set; }
+
+}
