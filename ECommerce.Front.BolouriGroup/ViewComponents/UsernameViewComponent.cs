d987c2ff415295995f382b8c09052bdb650b7894
alireza sayyah <sayyah.alireza@gmail.com>
2023-04-09 05:07:17 +0330

    [04-09-2023 Boloori] add user full name

---
+++ ECommerce.Front.BolouriGroup/ViewComponents/UsernameViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Services.IServices;
+using ECommerce.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Front.BolouriGroup.ViewComponents
+{
+    public class UsernameViewComponent : ViewComponent
+    {
+        private readonly ICookieService _cookieService;
+
+        public UsernameViewComponent(ICookieService cookieService)
+        {
+            _cookieService = cookieService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = _cookieService.GetCurrentUser();
+            return View(result);
+        }
+    }
+}
