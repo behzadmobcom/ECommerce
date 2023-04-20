742ad74ac1d68368ba3613c348c50d52089d546a
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2023-04-20 16:29:50 +0430

    [boloori]loginpage

---
+++ ECommerce.Front.BolouriGroup/Pages/NewLogin.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.Entities.Helper;
+using Ecommerce.Entities.ViewModel;
+using ECommerce.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZarinpalSandbox;
+
+namespace ECommerce.Front.BolouriGroup.Pages;
+
+public class NewLoginModel : PageModel
+{
+    private readonly IUserService _userService;
+
+    public NewLoginModel(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [BindProperty] public RegisterViewModel RegisterViewModel { get; set; }
+
+    [BindProperty] public LoginViewModel LoginViewModel { get; set; }
+
+    [TempData] public string Message { get; set; }
+
+    [TempData] public string Code { get; set; }
+
+    [TempData] public string ReturnUrl { get; set; }
+
+    public void OnGet(string returnUrl = null)
+    {
+        if (string.IsNullOrEmpty(returnUrl)) returnUrl = "/";
+        ReturnUrl = returnUrl;
+    }
+
+ 
+
+    
+ 
+
+  
+}
