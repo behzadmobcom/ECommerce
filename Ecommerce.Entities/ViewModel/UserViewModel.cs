c10ce1b46fc402815892fb35dadd86e7573bc774
alimozafari67 <a.mozafari67@gmail.com>
2023-06-19 22:29:07 +0330

    Resolve Varizi Warnings

--- Ecommerce.Entities/ViewModel/UserViewModel.cs
+++ Ecommerce.Entities/ViewModel/UserViewModel.cs
@@ -10,10 +10,10 @@ public class UserViewModel
 public class UserListViewModel
 {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string UserRole { get; set; }
-    public string State { get; set; }
-    public string City { get; set; }
+    public string? Username { get; set; }
+    public string? UserRole { get; set; }
+    public string? State { get; set; }
+    public string? City { get; set; }
     public decimal BuyingAmount { get; set; }
     public DateTime RegisterDate { get; set; }
     public bool IsActive { get; set; } = false;
@@ -21,7 +21,7 @@ public class UserListViewModel
 }
 public class UserFilterdParameters
 {
-    public PaginationParameters PaginationParameters { get; set; }
+    public PaginationParameters? PaginationParameters { get; set; }
     public UserSort UserSort { get; set; }
     public bool? IsColleauge { get; set; }
     public bool? HasBuying { get; set; }
