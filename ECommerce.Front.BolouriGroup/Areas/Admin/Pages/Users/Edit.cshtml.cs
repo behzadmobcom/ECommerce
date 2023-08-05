7f08af59bf53ca87e980004feea7a8f12f23abed
Saman <samanmozhdehi@gmail.com>
2023-08-05 09:43:08 +0330

    fix edit user problem

--- ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Users/Edit.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Users/Edit.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Entities;
+using Ecommerce.Entities.Helper;
 using ECommerce.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -27,12 +28,27 @@ public class EditModel : PageModel
     {
         if (ModelState.IsValid)
         {
-            var result = await _userService.Edit(User);
+            var resultUser = await _userService.GetById(User.Id);
+            var editUser = new User();
+            if (resultUser.Code == ServiceCode.Success)
+            {
+                editUser = resultUser.ReturnData;
+            }
+            editUser.UserName = User.UserName;
+            editUser.FirstName = User.FirstName;
+            editUser.LastName = User.LastName;
+            editUser.NationalCode = User.NationalCode;
+            editUser.Email = User.Email;
+            editUser.Mobile = User.Mobile;
+            editUser.PhoneNumber = User.PhoneNumber;
+            editUser.State = User.State;
+            editUser.IsActive = User.IsActive;
+            var result = await _userService.Update(editUser);
             Message = result.Message;
             Code = result.Code.ToString();
             if (result.Code == 0)
                 return RedirectToPage("/Users/Index",
-                    new { area = "Admin", message = result.Message, code = result.Code.ToString() });
+                    new { area = "Admin", message = result.Message, code = result.Code });
             Message = result.Message==null ? "پیغام خطای غیرمنتظره" : result.Message;
             Code = result.Code.ToString();
             ModelState.AddModelError("", Message);
