8d99b452a16e2c339a37736b86a660f1a8e5b0c1
Pourya Pourbagheri <pouryapb77@gmail.com>
2023-08-21 00:44:43 +0330

    Fixes #256 Discount

--- ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Discounts/Create.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Discounts/Create.cshtml.cs
@@ -15,6 +15,7 @@ public class CreateModel : PageModel
     }
 
     [BindProperty] public Discount Discount { get; set; }
+    [BindProperty] public bool WithPrice { get; set; }
 
     [TempData] public string Message { get; set; }
 
@@ -26,12 +27,21 @@ public class CreateModel : PageModel
 
     public async Task<IActionResult> OnPost()
     {
+        if (WithPrice && !Discount.Amount.HasValue) ModelState.AddModelError(string.Empty, "مبلغ نباید خالی باشد.");
+        if (!WithPrice && !Discount.Percent.HasValue) ModelState.AddModelError(string.Empty, "درصد نباید خالی باشد.");
+        if (!Discount.StartDate.HasValue) ModelState.AddModelError(string.Empty, "تاریخ شروع نباید خالی باشد.");
+        if (!Discount.EndDate.HasValue) ModelState.AddModelError(string.Empty, "تاریخ پایان نباید خالی باشد.");
+        if (Discount.StartDate > Discount.EndDate) ModelState.AddModelError(string.Empty, "تاریخ پایان نباید قبل از تاریخ شروع باشد.");
+        if (Discount.MinOrder > Discount.MaxOrder) ModelState.AddModelError(string.Empty, "حداقل تعداد سفارش باید کم تر از حداکثر آن باشد.");
+
         if (ModelState.IsValid)
         {
+            if (WithPrice) Discount.Percent = null;
+            else Discount.Amount = null;
             var result = await _discountService.Add(Discount);
             if (result.Code == 0)
                 return RedirectToPage("/Discounts/Index",
-                    new {area = "Admin", message = result.Message, code = result.Code.ToString()});
+                    new { area = "Admin", message = result.Message, code = result.Code.ToString() });
             Message = result.Message;
             Code = result.Code.ToString();
             ModelState.AddModelError("", result.Message);
--- ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Discounts/Edit.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Discounts/Edit.cshtml.cs
@@ -15,6 +15,7 @@ public class EditModel : PageModel
     }
 
     [BindProperty] public Discount Discount { get; set; }
+    [BindProperty] public bool WithPrice { get; set; }
     [TempData] public string Message { get; set; }
     [TempData] public string Code { get; set; }
 
@@ -22,18 +23,29 @@ public class EditModel : PageModel
     {
         var result = await _discountService.GetById(id);
         Discount = result.ReturnData;
+        if (!Discount.Amount.HasValue) WithPrice = false;
+        else if (!Discount.Percent.HasValue) WithPrice = true;
     }
 
     public async Task<IActionResult> OnPost()
     {
+        if (WithPrice && !Discount.Amount.HasValue) ModelState.AddModelError(string.Empty, "مبلغ نباید خالی باشد.");
+        if (!WithPrice && !Discount.Percent.HasValue) ModelState.AddModelError(string.Empty, "درصد نباید خالی باشد.");
+        if (!Discount.StartDate.HasValue) ModelState.AddModelError(string.Empty, "تاریخ شروع نباید خالی باشد.");
+        if (!Discount.EndDate.HasValue) ModelState.AddModelError(string.Empty, "تاریخ پایان نباید خالی باشد.");
+        if (Discount.StartDate > Discount.EndDate) ModelState.AddModelError(string.Empty, "تاریخ پایان نباید قبل از تاریخ شروع باشد.");
+        if (Discount.MinOrder > Discount.MaxOrder) ModelState.AddModelError(string.Empty, "حداقل تعداد سفارش باید کم تر از حداکثر آن باشد.");
+
         if (ModelState.IsValid)
         {
+            if (WithPrice) Discount.Percent = null;
+            else Discount.Amount = null;
             var result = await _discountService.Edit(Discount);
             Message = result.Message;
             Code = result.Code.ToString();
             if (result.Code == 0)
                 return RedirectToPage("/Discounts/Index",
-                    new {area = "Admin", message = result.Message, code = result.Code.ToString()});
+                    new { area = "Admin", message = result.Message, code = result.Code.ToString() });
             Message = result.Message;
             Code = result.Code.ToString();
             ModelState.AddModelError("", result.Message);
