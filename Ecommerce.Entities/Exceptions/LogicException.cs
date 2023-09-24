bcc1ebb0a7bedfec3d68f49f1daa4aa9e2c15852
alimozafari67 <a.mozafari67@gmail.com>
2023-09-24 21:39:44 +0330

    ID-412 ExeptionHandling

---
+++ Ecommerce.Entities/Exceptions/LogicException.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce.Entities.Helper;
+using System;
+
+namespace Common.Exceptions
+{
+    public class LogicException : AppException
+    {
+        public LogicException() 
+            : base(ResultCode.DatabaseError)
+        {
+        }
+
+        public LogicException(string message) 
+            : base(ResultCode.DatabaseError, message)
+        {
+        }
+
+        public LogicException(object additionalData) 
+            : base(ResultCode.DatabaseError, additionalData)
+        {
+        }
+
+        public LogicException(string message, object additionalData) 
+            : base(ResultCode.DatabaseError, message, additionalData)
+        {
+        }
+
+        public LogicException(string message, Exception exception)
+            : base(ResultCode.DatabaseError, message, exception)
+        {
+        }
+
+        public LogicException(string message, Exception exception, object additionalData)
+            : base(ResultCode.DatabaseError, message, exception, additionalData)
+        {
+        }
+    }
+}
