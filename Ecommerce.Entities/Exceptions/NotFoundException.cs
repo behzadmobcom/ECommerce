2a19808a07d36d3e06318a3ff81b4fa07c1b865e
alimozafari67 <a.mozafari67@gmail.com>
2023-09-24 21:39:44 +0330

    ID-412 ExeptionHandling

---
+++ Ecommerce.Entities/Exceptions/NotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce.Entities.Helper;
+using System;
+
+namespace Common.Exceptions
+{
+    public class NotFoundException : AppException
+    {
+        public NotFoundException()
+            : base(ResultCode.NotFound)
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(ResultCode.NotFound, message)
+        {
+        }
+
+        public NotFoundException(object additionalData)
+            : base(ResultCode.NotFound, additionalData)
+        {
+        }
+
+        public NotFoundException(string message, object additionalData)
+            : base(ResultCode.NotFound, message, additionalData)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception)
+            : base(ResultCode.NotFound, message, exception)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception, object additionalData)
+            : base(ResultCode.NotFound, message, exception, additionalData)
+        {
+        }
+    }
+}
