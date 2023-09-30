3a3c771d05656f4967953bfbaf1650ed801fa965
alireza ahmadi <alireza.ahmadi.araku@gmail.com>
2023-09-30 16:51:37 +0330

    ID-NewPaymentMethod

--- Ecommerce.Entities/PaymentMethod.cs
+++ Ecommerce.Entities/PaymentMethod.cs
@@ -4,9 +4,11 @@ namespace Ecommerce.Entities;
 
 public class PaymentMethod : BaseEntity
 {
-    [StringLength(50)]
+    [StringLength(26,MinimumLength =8,ErrorMessage ="حداقل 8 و حداکثر 26 کاراکتر")]
     [Display(Name = "شماره حساب")]
     [Required(ErrorMessage = @"{0} را وارد کنید")]
+    [RegularExpression("^[0-9]*$", ErrorMessage = "لطفا برای {0} فقط عدد وارد کنید")]
+
     public string AccountNumber { get; set; }
 
     [Display(Name = "نام بانک")]
@@ -20,6 +22,8 @@ public class PaymentMethod : BaseEntity
 
     [Display(Name = "کد بانک")]
     [StringLength(2)]
+    [RegularExpression("^[0-9]*$", ErrorMessage = "لطفا برای {0} فقط عدد وارد کنید")]
+
     public string? BankCode { get; set; }
 
     public PaymentMethodStatus PaymentMethodStatus { get; set; }
