c0f50bdaa6bffc71c6809675f37ca0c2692d018e
najafizade.mona@gmail.com <najafizade.mona@gmail.com>
2023-10-01 09:36:34 +0330

    ID-435 UnitTest

---
+++ ECommerce.Repository.UnitTests/Brands/BrandTests.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.Entities;
+using ECommerce.API.DataContext;
+using ECommerce.API.Repository;
+using Xunit;
+
+namespace ECommerce.Repository.UnitTests.Brands
+{
+    public class BrandTests
+    {
+        private readonly SunflowerECommerceDbContext _dbContext;
+
+        public BrandTests()
+        {
+            var db = new DbContextFake();
+            _dbContext = db.GetDatabaseContext();
+        }
+
+        [Fact]
+        public async Task AddAsync_AddNewEntity_ReturnsNewEntity()
+        {
+            //Arrange
+            int id = 1;
+            string name = "Brand for test";
+            var brandRepository = new BrandRepository(_dbContext);
+            Brand brand = new Brand
+            {
+                Id = id,
+                Name = name
+            };
+
+            //Act
+            var newBrand = await brandRepository.AddAsync(brand,new CancellationToken());
+
+            //Assert
+            Assert.Equal(newBrand.Id,id);
+            Assert.Equal(newBrand.Name,name);
+        }
+    }
+}
