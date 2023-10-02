3a0ea1811d7950198f3fc696731d94428eee5162
Pourya Pourbagheri <pouryapb77@gmail.com>
2023-10-02 21:37:29 +0330

    admin secondary menu fix Fixes #449

--- ECommerce.Front.BolouriGroup/wwwroot/Admin/js/loadScript.js
+++ ECommerce.Front.BolouriGroup/wwwroot/Admin/js/loadScript.js
@@ -1,9 +1,15 @@
 window.addEventListener("load", () => {
   const pathname = window.location.pathname;
   const currentListItem = $(`#kt_aside_menu_wrapper a[href='${pathname}']`);
+  const secondaryListItem = $(`#kt_header_menu a[href='${pathname}']`);
   if (currentListItem.length === 1) {
     const currentMainListParent = currentListItem.parent().parent().parent().parent();
     currentMainListParent.toggleClass("menu-item-open");
     currentListItem.parent().css("background-color", "#2b2b3d");
+  }
+  if (secondaryListItem.length === 1) {
+    secondaryListItem.closest(".menu-item-submenu").toggleClass("menu-item-open");
+    secondaryListItem.children().css("color", "#15a9cd");
+    secondaryListItem.parent().css("background-color", "#0000000f");
   }
 });
