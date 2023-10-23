aa020a73a871cb550b7db860bf7a4b49b6ac56e8
alireza sayyah <sayyah.alireza@gmail.com>
2023-10-23 23:59:58 +0330

    Fixed #513 Benchmark for controller

---
+++ Ecommerce.Benchmark/APIsBenchmarks/ColorAPIBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using ECommerce.Benchmark.RestAPIs;
+
+namespace ECommerce.Benchmark.APIsBenchmarks
+{
+    [HtmlExporter]
+    public class ColorApiBenchmarks
+    {
+        [Params(100, 200)]
+        public int IterationCount;
+
+        private readonly ColorsController _colorsController = new ColorsController();
+
+        [Benchmark]
+        public async Task RestGetSmallPayloadAsync()
+        {
+            for (int i = 0; i < IterationCount; i++)
+            {
+                await _colorsController.GetAllAsync();
+            }
+        }
+    }
+}
---
+++ Ecommerce.Benchmark/RestAPIs/ColorsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Net.Http.Json;
+
+namespace ECommerce.Benchmark.RestAPIs
+{
+    public class ColorsController : BaseController
+    {
+        public async Task<Color> GetAllAsync()
+        {
+            return await Client.GetFromJsonAsync<Color>($"https://localhost:7257/api/Colors/GetAll");
+        }
+    }
+}
