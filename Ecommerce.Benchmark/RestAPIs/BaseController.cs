6940b8fdc71fe02d5ebbaab20614408d53e51364
alireza sayyah <sayyah.alireza@gmail.com>
2023-10-23 23:59:58 +0330

    Fixed #513 Benchmark for controller

---
+++ Ecommerce.Benchmark/RestAPIs/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+namespace ECommerce.Benchmark.RestAPIs
+{
+    public class BaseController
+    {
+        public static readonly HttpClient Client = new();
+
+        public BaseController()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
