f2c0d903a540ae2e99a6fe4f62f2c31433421931
Pourya Pourbagheri <pouryapb77@gmail.com>
2023-10-31 22:29:35 +0330

    refactored shop.js

--- ECommerce.Front.BolouriGroup/wwwroot/js/shop.ts
+++ ECommerce.Front.BolouriGroup/wwwroot/js/shop.ts
@@ -1,5 +1,6 @@
 const isShopPage = window.location.pathname.split("/")[1].toLowerCase() === "shop";
 const linearProgress = '<div class="linear-activity"><div class="indeterminate"></div></div>';
+let isLoading = false;
 
 window.onpopstate = (e) => {
   if (!e.state) return;
@@ -10,10 +11,10 @@ const shopPaginationHandler = (e: Event, isClickable?: boolean) => {
   if (!isShopPage || !isClickable) return;
   e.preventDefault();
 
-  const url = (e.target as Element).getAttribute("href");
+  const url = new URL((e.target as Element).getAttribute("href"), window.location.origin);
   updatePage(url, false, true);
   if (!window.history.state) window.history.replaceState(window.location.pathname, "", window.location.pathname + window.location.search);
-  window.history.pushState(url, "", url);
+  window.history.pushState(url.href, "", url);
 };
 
 const shopRecordsCountHandler = (e: Event) => {
@@ -21,11 +22,12 @@ const shopRecordsCountHandler = (e: Event) => {
   e.preventDefault();
   const count = (e.target as Element).innerHTML;
 
-  const search = window.location.search === "" ? "?pageSize=20" : window.location.search;
-  const url = (window.location.pathname + search).replace(/pageSize=[0-9]+/g, `pageSize=${count}`).replace(/pageNumber=[0-9]+/g, "pageNumber=1");
+  const url = new URL(window.location.href);
+  url.searchParams.set("pageSize", count);
+  url.searchParams.set("pageNumber", "1");
   updatePage(url, false, true);
   if (!window.history.state) window.history.replaceState(window.location.pathname, "", window.location.pathname + window.location.search);
-  window.history.pushState(url, "", url);
+  window.history.pushState(url.href, "", url);
 };
 
 const attachToPagination = () =>
@@ -44,25 +46,30 @@ if (isShopPage) {
 
 /**
  *
- * @param base Base URL or complete URL with query.
+ * @param url
  * @param handler Which handler method to use?
  * @param firstPage Should try loading first page?
  * @param hasQuery If url already has query string attached on it?
  * @returns
  */
-const getShopHandler = async (base: string, handler: "products" | "counts" | "pagination", firstPage?: boolean, hasQuery?: boolean) => {
+const getShopHandler = async (url: URL, handler: "products" | "counts" | "pagination", firstPage?: boolean, hasQuery?: boolean) => {
   if (!isShopPage) return;
-  let search = hasQuery ? "?" + base.split("?")[1] : window.location.search;
-  if (search === "") search += `?handler=${handler}`;
-  else search += `&handler=${handler}`;
-  if (firstPage) search = search.replace(/pageNumber=[0-9]+/g, "pageNumber=1");
-  if (hasQuery) base = base.split("?")[0];
-  search = search.replace(/(search=).*?(&|$)/g, "$1$2");
-  return await $.get(base + search);
+
+  if (!hasQuery)
+    new URLSearchParams(window.location.search).forEach((value, key) => {
+      url.searchParams.set(key, value);
+    });
+
+  url.searchParams.set("handler", handler);
+  if (firstPage) url.searchParams.set("pageNumber", "1");
+  url.searchParams.delete("search");
+  return await $.get(url.href);
 };
 
 const updateFilterLink = () => {
-  $("#shop-widget-category form").attr("action", (_, value) => `${window.location.pathname}?${value.split("?")[1]}`);
+  const filterForm = $("#shop-widget-category form");
+  filterForm.attr("action", (_, value) => `${window.location.pathname}?${value.split("?")[1]}`);
+  filterForm.find("input[name='search']").removeAttr("value");
 };
 
 /**
@@ -74,16 +81,30 @@ const categoryChangeHandler = async (e: Event | string) => {
   if (!isShopPage) return;
   const isState = typeof e === "string";
   if (!isState) e.preventDefault();
+  if (isLoading) return;
+  isLoading = true;
 
   $("#searchBox").val("");
 
-  const url = !isState ? (e.target as Element).getAttribute("href") : e;
-  updatePage(url, !isState);
-  if (!window.history.state)
-    window.history.replaceState(window.location.pathname, "", window.location.pathname + window.location.search.replace(/(search=).*?(&|$)/g, "$1$2"));
-  if (!isState)
-    window.history.pushState(url, "", url + window.location.search.replace(/pageNumber=[0-9]+/g, "pageNumber=1").replace(/(search=).*?(&|$)/g, "$1$2"));
+  const url = new URL(!isState ? (e.target as Element).getAttribute("href") : e, window.location.origin);
+  await updatePage(url, !isState).catch((err) => {
+    console.log(err);
+    $(".linear-activity").remove();
+    isLoading = false;
+  });
+  const currentPageUrl = new URL(window.location.pathname + window.location.search, window.location.origin);
+  currentPageUrl.searchParams.delete("search");
+  if (!window.history.state) {
+    window.history.replaceState(currentPageUrl.pathname, "", currentPageUrl.pathname + currentPageUrl.search);
+  }
+  if (!isState) {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("pageNumber", "1");
+    searchParams.delete("search");
+    window.history.pushState(url.pathname, "", url.pathname + "?" + searchParams.toString());
+  }
   updateFilterLink();
+  isLoading = false;
 };
 
 /**
@@ -92,15 +113,16 @@ const categoryChangeHandler = async (e: Event | string) => {
  * @param firstPage Should try loading first page?
  * @param noSearch Should use window.history.search?
  */
-const updatePage = async (url: string, firstPage?: boolean, noSearch?: boolean) => {
+const updatePage = async (url: URL, firstPage?: boolean, noSearch?: boolean) => {
   const container = $("#productsContainer");
   const currentItems = $(".category-item-selected");
+  const copyOfUrl = new URL(url);
 
   container.before(linearProgress);
   const [shopList, shopCount, shopPagination] = await Promise.all([
-    getShopHandler(url, "products", firstPage, noSearch),
-    getShopHandler(url, "counts", firstPage, noSearch),
-    getShopHandler(url, "pagination", firstPage, noSearch),
+    getShopHandler(copyOfUrl, "products", firstPage, noSearch),
+    getShopHandler(copyOfUrl, "counts", firstPage, noSearch),
+    getShopHandler(copyOfUrl, "pagination", firstPage, noSearch),
   ]);
 
   container.prev().remove();
@@ -110,7 +132,7 @@ const updatePage = async (url: string, firstPage?: boolean, noSearch?: boolean) 
   $(".pagination").html(shopPagination);
 
   currentItems.removeClass();
-  const baseUrl = url.split("?")[0];
+  const baseUrl = url.pathname;
   $(`#shop-widget-category a[href='${baseUrl}'], #category-layout a[href='${baseUrl}'], #category-nav a[href='${baseUrl}']`).addClass("category-item-selected");
   attachToPagination();
 };
